0e9a57115de9f338a64984809a7b33cca82c824e
airwarriorg91 <gauravxpgupta@gmail.com>
2022-06-23 21:54:00 +0530

    'edit'

--- conscientia2k22/settings.py
+++ conscientia2k22/settings.py
@@ -133,7 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    #BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
