f54112b032c60550ac26ffc406a1c9b20cfa5efe
airwarriorg91 <gauravxpgupta@gmail.com>
2022-06-23 19:47:36 +0530

    'edit'

--- conscientia2k22/urls.py
+++ conscientia2k22/urls.py
@@ -1,7 +1,7 @@
-rom django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('home.urls')),
 ]
